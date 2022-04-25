8449e160759ade4c6fc090368f75061f0c5fb45d
amigin <amigin@gmail.com>
2022-04-25 11:51:25 +0300

    Added PKname

--- src/fn_impl_insert_or_update/generate.rs
+++ src/fn_impl_insert_or_update/generate.rs
@@ -13,7 +13,7 @@ pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
     result.push_str(struct_name.as_str());
     result.push_str(" {\n");
 
-    result.push_str("pub async fn insert_db_entity(&self, client: &tokio_postgres::Client, table_name: &str,) -> Result<(), tokio_postgres::Error> {");
+    result.push_str("pub async fn insert_or_update_db_entity(&self, client: &tokio_postgres::Client, table_name: &str, pk_name: &str,) -> Result<(), tokio_postgres::Error> {");
     super::fn_insert_or_update::fn_insert_or_update(&mut result, &fields);
     result.push_str("}\n");
 
