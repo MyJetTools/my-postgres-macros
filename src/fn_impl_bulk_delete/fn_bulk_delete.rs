44aab1300c084343001603d14c5cb4efbdd39d95
amigin <amigin@gmail.com>
2022-06-28 17:48:38 +0300

    Fixing BulkDelete

--- src/fn_impl_bulk_delete/fn_bulk_delete.rs
+++ src/fn_impl_bulk_delete/fn_bulk_delete.rs
@@ -4,14 +4,15 @@ pub fn fn_bulk_delete(result: &mut String, fields: &[StructProperty]) {
     result.push_str("let mut sql_builder = my_postgres_utils::delete::BulkDeleteBuilder::new();");
 
     result.push_str("for entity in entities {");
-
+    result.push_str("sql_builder.add_new_line();");
     for property in fields {
-        result.push_str("sql_builder.add_new_line();");
-        crate::postgres_utils::read_value(result, property);
+        if property.is_primary_key() {
+            crate::postgres_utils::read_value(result, property);
 
-        result.push_str("sql_builder.add_where_field(\"");
-        result.push_str(property.get_db_field_name());
-        result.push_str("\");");
+            result.push_str("sql_builder.add_where_field(\"");
+            result.push_str(property.get_db_field_name());
+            result.push_str("\", sql_value);");
+        }
     }
 
     result.push_str("}");
