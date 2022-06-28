e383f184abdac5980fb1dc09b7e44c2b422b31af
amigin <amigin@gmail.com>
2022-06-28 22:47:04 +0300

    Fixing Update

--- src/fn_impl_update/fn_update.rs
+++ src/fn_impl_update/fn_update.rs
@@ -9,7 +9,7 @@ pub fn fn_update(result: &mut String, fields: &[StructProperty]) {
 }
 
 fn fn_update_with_ignore_fields(result: &mut String, fields: &[StructProperty]) {
-    result.push_str("let mut sql = my_postgres_utils::PosrgresUpdateBuilder::new();\n");
+    result.push_str("let mut sql = my_postgres_utils::update::UpdateBuilder::new();\n");
 
     crate::postgres_utils::generate_field_names_runtime(
         result,
