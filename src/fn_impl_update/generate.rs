d428e3ebda4b237501720e80b39b69733a4a5794
amigin <amigin@gmail.com>
2022-06-28 22:44:40 +0300

    Fixing  bug

--- src/fn_impl_update/generate.rs
+++ src/fn_impl_update/generate.rs
@@ -13,7 +13,7 @@ pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
     result.push_str(struct_name.as_str());
     result.push_str(" {\n");
 
-    result.push_str("pub async fn update_db_entity(&self, client: &tokio_postgres::Client, table_name: &str,) -> Result<(), tokio_postgres::Error> {");
+    result.push_str("pub async fn update_db_entity(self, client: &tokio_postgres::Client, table_name: &str,) -> Result<(), tokio_postgres::Error> {");
     super::fn_update::fn_update(&mut result, &fields);
     result.push_str("}\n");
 
