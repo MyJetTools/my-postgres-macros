c3973eec6ef8a3f88bd4918e9fb4bc5b27374578
amigin <amigin@gmail.com>
2022-06-29 18:58:03 +0300

    Fixing bug

--- src/fn_impl_reading/fn_from_db_row.rs
+++ src/fn_impl_reading/fn_from_db_row.rs
@@ -3,7 +3,7 @@ use crate::reflection::StructProperty;
 pub fn fn_from_db_row(result: &mut String, fields: &[StructProperty]) {
     for prop in fields {
         if prop.ty.is_date_time() {
-            result.push_str("let dt: DateTime<Utc> = ");
+            result.push_str("let dt: chrono::DateTime<chrono::Utc> = ");
             generate_read_db_row_field(result, prop);
             result.push_str(";\n");
 
