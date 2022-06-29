9ad249de2f2a4dcc30f5f7a67574df98e3455417
amigin <amigin@gmail.com>
2022-06-29 20:58:12 +0300

    Fixing bug

--- src/fn_impl_reading/fn_from_db_row.rs
+++ src/fn_impl_reading/fn_from_db_row.rs
@@ -31,15 +31,19 @@ pub fn fn_from_db_row(result: &mut String, fields: &[StructProperty]) {
     for prop in fields {
         if prop.ty.is_date_time() {
             result.push_str(prop.name.as_str());
-        } else if let PropertyType::OptionOf(sub_ty) = &prop.ty {
+            result.push_str(",\n");
+            continue;
+        }
+        if let PropertyType::OptionOf(sub_ty) = &prop.ty {
             if sub_ty.is_date_time() {
                 result.push_str(prop.name.as_str());
+                result.push_str(",\n");
+                continue;
             }
-        } else {
-            result.push_str(prop.name.as_str());
-            result.push_str(": ");
-            generate_read_db_row_field(result, prop);
         }
+        result.push_str(prop.name.as_str());
+        result.push_str(": ");
+        generate_read_db_row_field(result, prop);
 
         result.push_str(",\n");
     }
