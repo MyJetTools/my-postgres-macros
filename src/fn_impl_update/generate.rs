b8e72fc20988d1ed6a20b3e800dcb375386055ff
amigin <amigin@gmail.com>
2022-07-04 20:11:55 +0300

    Fixing

--- src/fn_impl_update/generate.rs
+++ src/fn_impl_update/generate.rs
@@ -13,7 +13,9 @@ pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
     result.push_str(struct_name.as_str());
     result.push_str(" {\n");
 
-    result.push_str("fn update_db_entity(self, sql_builder: &mut my_postgres::code_gens::update::UpdateBuilder){");
+    result.push_str(
+        "fn populate(self, sql_builder: &mut my_postgres::code_gens::update::UpdateBuilder){",
+    );
     super::fn_update::fn_update(&mut result, &fields);
     result.push_str("}\n");
 
