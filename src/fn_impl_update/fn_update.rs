cf7ed8389862305b0707db513b057ffd64175d35
amigin <amigin@gmail.com>
2022-07-04 22:48:28 +0300

    Fixing some bugs

--- src/fn_impl_update/fn_update.rs
+++ src/fn_impl_update/fn_update.rs
@@ -3,7 +3,7 @@ use crate::reflection::StructProperty;
 pub fn fn_update(result: &mut String, fields: &[StructProperty]) {
     for property in fields {
         if property.has_ignore_if_null_attr() {
-            result.push_str("if let Some(value) = &self.");
+            result.push_str("if let Some(value) = self.");
             result.push_str(&property.name);
             result.push_str("{");
             crate::postgres_utils::read_value(result, property, "value");
