2d362368f4c787ee693c3931079b1680590a3d58
amigin <amigin@gmail.com>
2022-07-13 23:56:36 +0300

    Fixing Bugs

--- src/fn_impl_insert/generate.rs
+++ src/fn_impl_insert/generate.rs
@@ -14,7 +14,7 @@ pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
     result.push_str(" {\n");
 
     result.push_str(
-        "fn populate(self, sql_builder: &mut my_postgres::code_gens::insert::InsertCodeGen){",
+        "fn populate(self, sql_builder: &mut dyn my_postgres::code_gens::insert::InsertCodeGen){",
     );
     super::fn_insert::fn_insert(&mut result, &fields);
     result.push_str("}\n");
