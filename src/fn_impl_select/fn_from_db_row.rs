b3c382f6b6ec6f822ca7933b0439a20a6c40a9b4
amigin <amigin@gmail.com>
2022-10-11 15:37:35 +0300

    Fixing Bugs

--- src/fn_impl_select/fn_from_db_row.rs
+++ src/fn_impl_select/fn_from_db_row.rs
@@ -3,14 +3,16 @@ use crate::reflection::{PropertyType, StructProperty};
 pub fn fn_from_db_row(result: &mut String, fields: &[StructProperty]) {
     for prop in fields {
         if prop.ty.is_date_time() {
-            result.push_str("let dt: chrono::NaiveDateTime = ");
-            generate_read_db_row_field(result, prop);
-            result.push_str(";\n");
+            if prop.has_timestamp_attr() {
+                result.push_str("let dt: chrono::NaiveDateTime = ");
+                generate_read_db_row_field(result, prop);
+                result.push_str(";\n");
 
-            ///////////
-            result.push_str("let ");
-            result.push_str(prop.name.as_str());
-            result.push_str(" = DateTimeAsMicroseconds::new(dt.timestamp_millis() * 1000);");
+                ///////////
+                result.push_str("let ");
+                result.push_str(prop.name.as_str());
+                result.push_str(" = DateTimeAsMicroseconds::new(dt.timestamp_millis() * 1000);");
+            }
         }
         if let PropertyType::OptionOf(sub_ty) = &prop.ty {
             if sub_ty.is_date_time() {
