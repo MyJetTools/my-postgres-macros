f59d176f7e5f5c7e4930118401269237cd00d878
amigin <amigin@gmail.com>
2022-11-01 14:58:00 +0200

    debugging

--- src/postgres_utils.rs
+++ src/postgres_utils.rs
@@ -243,10 +243,9 @@ pub fn read_value(
         PropertyType::Struct(_) => {
             if property.has_json_attr() {
                 if sub_property.is_some() {
-                    // result.push_str("let sql_value = serde_json::to_string(&self.");
-                    // result.push_str(property.name.as_str());
-                    // result.push_str(").unwrap();");
-                    result.push_str("todo!();");
+                    result.push_str("serde_json::to_string(&self.");
+                    result.push_str(property.name.as_str());
+                    result.push_str(").unwrap();");
                 } else {
                     result.push_str("serde_json::to_string(&self.");
                     result.push_str(property.name.as_str());
