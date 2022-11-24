e54e98acdd4e724f120f49642ec3c8b2ccb63af8
amigin <amigin@gmail.com>
2022-11-24 11:54:13 +0200

    Fixing

--- src/fn_impl_update/generate.rs
+++ src/fn_impl_update/generate.rs
@@ -1,5 +1,7 @@
 use proc_macro::TokenStream;
-use types_reader::StructProperty;
+use types_reader::{PropertyType, StructProperty};
+
+use crate::postgres_utils::PostgresStructPropertyExt;
 
 pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
@@ -8,16 +10,34 @@ pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
 
     let struct_name = name.to_string();
 
+    let mut has_str = false;
+
+    for field in &fields {
+        if let PropertyType::Str = field.ty {
+            has_str = true;
+            break;
+        }
+    }
+
+    let mut primary_key_amount = 0;
+
+    for field in &fields {
+        if field.is_primary_key() {
+            primary_key_amount += 1;
+        }
+    }
+
     let mut result = String::new();
 
-    result.push_str("impl my_postgres::UpdateEntity for ");
+    result.push_str("impl my_postgres::sql_update::SqlUpdateModel for ");
     result.push_str(struct_name.as_str());
+    if has_str {
+        result.push_str("<'s>");
+    }
     result.push_str(" {\n");
 
-    result.push_str(
-        "fn populate<'s>(&'s self, sql_builder: &mut my_postgres::code_gens::update::UpdateBuilder<'s>){",
-    );
-    super::fn_update::fn_update(&mut result, &fields);
+    result.push_str("fn get_fields_amount() -> usize{");
+    result.push_str((fields.len() - primary_key_amount).to_string().as_str());
     result.push_str("}\n");
 
     result.push_str("}\n");
