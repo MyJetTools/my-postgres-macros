fd43f8dbe60e1cfc97f8e0379226b6540feec3c4
amigin <amigin@gmail.com>
2022-11-24 11:13:28 +0200

    Fixing

--- src/fn_impl_insert/generate.rs
+++ src/fn_impl_insert/generate.rs
@@ -10,7 +10,7 @@ pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
 
     let mut result = String::new();
 
-    result.push_str("impl<'s> my_postgres::SqlInsertModel<'s> for ");
+    result.push_str("impl<'s> my_postgres::sql_insert::SqlInsertModel<'s> for ");
     result.push_str(struct_name.as_str());
     result.push_str(" {\n");
 
