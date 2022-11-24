542f96063025013883ac2a22895a0bfe6bdd3a67
amigin <amigin@gmail.com>
2022-11-24 11:41:31 +0200

    Fixing

--- src/fn_impl_insert/fn_get_field_value.rs
+++ src/fn_impl_insert/fn_get_field_value.rs
@@ -6,7 +6,7 @@ pub fn fn_get_field_value(result: &mut String, fields: &[StructProperty]) {
     result.push_str("match no{");
     for (i, field) in fields.iter().enumerate() {
         result.push_str(i.to_string().as_str());
-        result.push_str("=> SqlValue::Value(");
+        result.push_str("=> my_postgres:: SqlValue::Value(");
         get_field_value(result, field);
         result.push_str("),");
     }
@@ -112,23 +112,15 @@ fn get_field_value_of_vec(
 }
 
 fn fill_sql_value(result: &mut String, struct_propery: &StructProperty) {
-    result.push_str("my_postgres::SqlWhereValue::AsValue { name: \"");
-    result.push_str(struct_propery.get_db_field_name());
-    result.push_str("\", value: Some(&self.");
+    result.push_str("Some(&self.");
     result.push_str(&struct_propery.name);
-    result.push_str("),");
-    fill_op(result, struct_propery);
-    result.push_str("}");
+    result.push_str(")");
 }
 
 fn fill_option_of_sql_value(result: &mut String, struct_propery: &StructProperty) {
-    result.push_str("my_postgres::SqlWhereValue::AsValue { name: \"");
-    result.push_str(struct_propery.get_db_field_name());
-    result.push_str("\", value: if let Some(value) = &self.");
+    result.push_str("if let Some(value) = &self.");
     result.push_str(&struct_propery.name);
-    result.push_str("{Some(value)}else{None},");
-    fill_op(result, struct_propery);
-    result.push_str("}");
+    result.push_str("{Some(value)}else{None}");
 }
 
 fn fill_option_of_vec_of_value(result: &mut String, struct_propery: &StructProperty) {
@@ -145,62 +137,4 @@ fn fill_vec_of_sql_value(result: &mut String, struct_propery: &StructProperty) {
     result.push_str("\", &Some(self.");
     result.push_str(&struct_propery.name);
     result.push_str("))");
-}
-
-fn fill_op(result: &mut String, struct_propery: &StructProperty) {
-    if let Some(op) = struct_propery.attrs.try_get("operator") {
-        if let Some(content) = op.content.as_ref() {
-            result.push_str("op: \"");
-            result.push_str(extract_and_verify_operation(content));
-            result.push_str("\"");
-        }
-        return;
-    }
-
-    if struct_propery.attrs.has_attr("ne") {
-        result.push_str("op: \"!=\"");
-        return;
-    }
-
-    result.push_str("op: \"=\"");
-}
-
-fn extract_and_verify_operation(src: &[u8]) -> &str {
-    let result = extract_operation(src);
-
-    if result == "="
-        || result == "!="
-        || result == "<"
-        || result == "<="
-        || result == ">"
-        || result == ">="
-        || result == "<>"
-    {
-        return result;
-    }
-
-    panic!("Invalid operator {}", result);
-}
-
-fn extract_operation(src: &[u8]) -> &str {
-    let src = &src[1..src.len() - 1];
-
-    for i in 0..src.len() {
-        if src[i] == b'"' || src[i] == b'\'' {
-            let b = src[i];
-
-            for j in 1..src.len() {
-                let pos = src.len() - j;
-
-                if src[pos] == b {
-                    let result = &src[i + 1..pos];
-
-                    let result = std::str::from_utf8(result).unwrap();
-                    return result;
-                }
-            }
-        }
-    }
-
-    std::str::from_utf8(src).unwrap()
 }
