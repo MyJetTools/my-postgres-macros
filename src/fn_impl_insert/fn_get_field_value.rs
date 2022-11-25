f319f5205d8fffbdb0e38975cddb8d6a471d9a85
amigin <amigin@gmail.com>
2022-11-25 09:45:08 +0200

    Fixing

--- src/fn_impl_insert/fn_get_field_value.rs
+++ src/fn_impl_insert/fn_get_field_value.rs
@@ -4,9 +4,9 @@ pub fn fn_get_field_value(result: &mut String, fields: &[StructProperty]) {
     result.push_str("match no{");
     for (i, field) in fields.iter().enumerate() {
         result.push_str(i.to_string().as_str());
-        result.push_str("=> my_postgres::SqlValue::Value(");
+        result.push_str("=> my_postgres::SqlValue::Value");
         crate::get_field_value::get_field_value(result, field);
-        result.push_str("),");
+        result.push_str(",");
     }
     result.push_str("_=>panic!(\"no such field with number {}\", no)");
     result.push_str("}");
