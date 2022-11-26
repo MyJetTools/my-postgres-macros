4d75cd7e0970051462c46a0a9a08f4901638d745
amigin <amigin@gmail.com>
2022-11-26 23:03:28 +0200

    Fixing

--- src/fn_impl_select/mod.rs
+++ src/fn_impl_select/mod.rs
@@ -12,7 +12,7 @@ fn fill_sql_type(result: &mut String, prop: &StructProperty) {
     if let Some(sql_type) = prop.get_sql_type() {
         result.push_str("Some(\"");
         result.push_str(sql_type.as_str());
-        result.push_str(")");
+        result.push_str("\")");
     } else {
         result.push_str("None");
     }
