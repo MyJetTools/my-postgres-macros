fdea79f87699f1a0b7215b1186492bb7d5a8448f
amigin <amigin@gmail.com>
2022-11-27 23:37:37 +0200

    Fixing BulkSelect

--- src/fn_impl_bulk_select/generate.rs
+++ src/fn_impl_bulk_select/generate.rs
@@ -10,7 +10,7 @@ pub fn generate(ast: &syn::DeriveInput) -> TokenStream {
 
     let mut result = String::new();
 
-    result.push_str("impl my_postgres::BulkSelectEntity for ");
+    result.push_str("impl my_postgres::sql_select::BulkSelectEntity for ");
     result.push_str(struct_name.as_str());
     result.push_str(" {\n");
 
