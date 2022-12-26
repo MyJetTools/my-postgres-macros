0f274d094793b9cc40f2a275a34d09b3dbf6c7a5
amigin <amigin@gmail.com>
2022-12-26 14:15:05 +0200

    Debugging

--- src/fn_impl_select/fn_fill_select_fields.rs
+++ src/fn_impl_select/fn_fill_select_fields.rs
@@ -1,14 +1,23 @@
-use types_reader::StructProperty;
+use types_reader::{PropertyType, StructProperty};
 
 use crate::postgres_utils::PostgresStructPropertyExt;
 use quote::quote;
 
 pub fn fn_fill_select_fields(fields: &[StructProperty]) -> Vec<proc_macro2::TokenStream> {
-    let mut result = Vec::with_capacity(fields.len());
+    let mut result = Vec::with_capacity(fields.len() * 2);
+    let mut no = 0;
     for prop in fields {
         if prop.attrs.has_attr("line_no") {
             continue;
         }
+
+        if no > 0 {
+            result.push(quote! {
+                sql.push(',');
+            });
+        }
+
+        no += 1;
 
         if let Some(sql) = prop.attrs.try_get("sql") {
             if let Some(value) = &sql.content {
@@ -24,16 +33,28 @@ pub fn fn_fill_select_fields(fields: &[StructProperty]) -> Vec<proc_macro2::Toke
                 );
             }
         } else {
-            let type_ident = prop.get_syn_type_as_token_stream();
             let db_field_name = prop.get_db_field_name();
+
             let sql_type = super::fill_sql_type(prop);
 
-            result.push(
-                quote! {
-                    #type_ident::fill_select_part(sql, #db_field_name, #sql_type);
-                }
-                .into(),
-            );
+            if let PropertyType::OptionOf(sub_type) = &prop.ty {
+                let type_ident = sub_type.get_token_stream();
+
+                result.push(
+                    quote! {
+                        #type_ident::fill_select_part(sql, #db_field_name, #sql_type);
+                    }
+                    .into(),
+                );
+            } else {
+                let type_ident = prop.ty.get_token_stream();
+                result.push(
+                    quote! {
+                        #type_ident::fill_select_part(sql, #db_field_name, #sql_type);
+                    }
+                    .into(),
+                );
+            }
         }
     }
 
