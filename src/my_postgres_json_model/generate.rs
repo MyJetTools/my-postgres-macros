556d19c43e0bdeca4b63f772da708cf1767dc681
amigin <amigin@gmail.com>
2022-12-29 13:32:24 +0200

    Fixes

--- src/my_postgres_json_model/generate.rs
+++ src/my_postgres_json_model/generate.rs
@@ -8,14 +8,14 @@ pub fn generate(ast: &syn::DeriveInput) -> proc_macro::TokenStream {
 
     quote! {
         impl my_postgres::sql_select::SelectPartValue for #struct_name {
-            fn fill_select_part(sql: &mut String, field_name: &str, _sql_type: Option<&str>) {
+            fn fill_select_part(sql: &mut String, field_name: &str, metadata: &Option<my_postgres::SqlValueMetadata>) {
                 sql.push_str(field_name);
                 sql.push_str("::text");
             }
         }
 
         impl my_postgres::sql_select::FromDbRow<#struct_name> for #struct_name {
-            fn from_db_row(row: &tokio_postgres::Row, name: &str, _sql_type: Option<&str>) -> #struct_name {
+            fn from_db_row(row: &tokio_postgres::Row, name: &str, metadata: &Option<my_postgres::SqlValueMetadata>) -> #struct_name {
                 let str_value: String = row.get(name);
                 serde_json::from_str(&str_value).unwrap()
             }
