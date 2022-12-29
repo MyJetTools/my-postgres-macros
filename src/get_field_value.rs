8c111864553baef984d203c80f3b3fe47ceb1593
amigin <amigin@gmail.com>
2022-12-29 11:43:49 +0200

    Fixing

--- src/get_field_value.rs
+++ src/get_field_value.rs
@@ -86,7 +86,7 @@ fn fill_option_of_value(struct_propery: &StructProperty) -> proc_macro2::TokenSt
 pub fn render_metadata(struct_propery: &StructProperty) -> proc_macro2::TokenStream {
     if let Some(sql_type) = struct_propery.get_sql_type() {
         return quote! {
-            Some(#sql_type)
+            Some(my_postgres::SqlValueMetadata::with_sql_type(#sql_type))
         }
         .into();
     }
