e6843991b30ad109128117038649b472e32c2657
amigin <amigin@gmail.com>
2022-12-31 17:06:12 +0200

    Fix

--- src/db_enum/generate.rs
+++ src/db_enum/generate.rs
@@ -111,7 +111,7 @@ pub fn generate(ast: &syn::DeriveInput, enum_type: EnumType) -> proc_macro::Toke
                 &'s self,
                 sql: &mut String,
                 params: &mut Vec<my_postgres::SqlValue<'s>>,
-                metadata: &Option<my_postgress::SqlValueMetadata>,
+                metadata: &Option<my_postgres::SqlValueMetadata>,
             ) {
                 sql.push_str(self.as_numbered_str());
             }
