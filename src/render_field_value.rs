1fa933d664352eaa3c2481d0aa544628a664148e
amigin <amigin@gmail.com>
2023-01-12 14:14:06 +0200

    Small Improvments

--- src/render_field_value.rs
+++ src/render_field_value.rs
@@ -1,53 +1,14 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use types_reader::{PropertyType, StructProperty};
+use types_reader::StructProperty;
 
 use crate::postgres_struct_ext::PostgresStructPropertyExt;
 
 pub fn render_field_value(struct_propery: &StructProperty) -> proc_macro2::TokenStream {
     match &struct_propery.ty {
-        types_reader::PropertyType::U8 => return get_value(struct_propery),
-        types_reader::PropertyType::I8 => return get_value(struct_propery),
-        types_reader::PropertyType::U16 => return get_value(struct_propery),
-        types_reader::PropertyType::I16 => return get_value(struct_propery),
-        types_reader::PropertyType::U32 => return get_value(struct_propery),
-        types_reader::PropertyType::I32 => return get_value(struct_propery),
-        types_reader::PropertyType::U64 => return get_value(struct_propery),
-        types_reader::PropertyType::I64 => return get_value(struct_propery),
-        types_reader::PropertyType::F32 => return get_value(struct_propery),
-        types_reader::PropertyType::F64 => return get_value(struct_propery),
-        types_reader::PropertyType::String => return get_value(struct_propery),
-        types_reader::PropertyType::Str => return get_value(struct_propery),
-        types_reader::PropertyType::DateTime => return get_value(struct_propery),
-        types_reader::PropertyType::Bool => return get_value(struct_propery),
-        types_reader::PropertyType::OptionOf(sub_type) => {
-            return fill_option_of(struct_propery, &sub_type)
-        }
+        types_reader::PropertyType::OptionOf(_) => return fill_option_of_value(struct_propery),
         types_reader::PropertyType::Struct(..) => return get_value(struct_propery),
         _ => return get_value(struct_propery),
-    }
-}
-
-fn fill_option_of(
-    struct_propery: &StructProperty,
-    sub_type: &PropertyType,
-) -> proc_macro2::TokenStream {
-    match &sub_type {
-        types_reader::PropertyType::U8 => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::I8 => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::U16 => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::I16 => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::U32 => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::I32 => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::U64 => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::I64 => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::F32 => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::F64 => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::String => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::Str => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::DateTime => return fill_option_of_value(struct_propery),
-        types_reader::PropertyType::Bool => return fill_option_of_value(struct_propery),
-        _ => panic!("{} is not supported", struct_propery.ty.as_str()),
     }
 }
 
