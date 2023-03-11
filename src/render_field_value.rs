2b225b28554562fc23b65b2a56075387f72fa8f5
amigin <amigin@gmail.com>
2023-03-11 10:57:46 +0200

    Fixed Misspelling

--- src/render_field_value.rs
+++ src/render_field_value.rs
@@ -5,22 +5,22 @@ use types_reader::StructProperty;
 use crate::postgres_struct_ext::PostgresStructPropertyExt;
 
 pub fn render_field_value(
-    struct_propery: &StructProperty,
+    struct_property: &StructProperty,
     is_update: bool,
 ) -> proc_macro2::TokenStream {
-    match &struct_propery.ty {
+    match &struct_property.ty {
         types_reader::PropertyType::OptionOf(_) => {
-            return fill_option_of_value(struct_propery, is_update)
+            return fill_option_of_value(struct_property, is_update)
         }
-        types_reader::PropertyType::Struct(..) => return get_value(struct_propery, is_update),
-        _ => return get_value(struct_propery, is_update),
+        types_reader::PropertyType::Struct(..) => return get_value(struct_property, is_update),
+        _ => return get_value(struct_property, is_update),
     }
 }
 
-fn get_value(struct_propery: &StructProperty, is_update: bool) -> proc_macro2::TokenStream {
-    let name = struct_propery.get_field_name_ident();
+fn get_value(struct_property: &StructProperty, is_update: bool) -> proc_macro2::TokenStream {
+    let name = struct_property.get_field_name_ident();
 
-    let metadata = struct_propery.get_field_metadata();
+    let metadata = struct_property.get_field_metadata();
 
     if is_update {
         quote! {
@@ -42,14 +42,14 @@ fn get_value(struct_propery: &StructProperty, is_update: bool) -> proc_macro2::T
 }
 
 fn fill_option_of_value(
-    struct_propery: &StructProperty,
+    struct_property: &StructProperty,
     is_update: bool,
 ) -> proc_macro2::TokenStream {
-    let prop_name = struct_propery.get_field_name_ident();
+    let prop_name = struct_property.get_field_name_ident();
 
-    let metadata = struct_propery.get_field_metadata();
+    let metadata = struct_property.get_field_metadata();
 
-    let else_case: TokenStream = if struct_propery.has_ignore_if_null_attr() {
+    let else_case: TokenStream = if struct_property.has_ignore_if_null_attr() {
         if is_update {
             quote!(my_postgres::SqlUpdateValueWrapper::Ignore).into()
         } else {
