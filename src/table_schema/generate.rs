2546f6cd4f0ef26d63a64ff69b3b943d07e23aca
amigin <amigin@gmail.com>
2023-03-31 18:28:16 +0300

    Fixes

--- src/table_schema/generate.rs
+++ src/table_schema/generate.rs
@@ -142,7 +142,7 @@ fn get_sql_type(
     field: &StructProperty,
     ty: &PropertyType,
 ) -> Result<proc_macro2::TokenStream, syn::Error> {
-    let ty_token = ty.get_token_stream();
+    let ty_token = ty.get_token_stream_with_generics();
 
     let meta_data = field.get_field_metadata();
 
