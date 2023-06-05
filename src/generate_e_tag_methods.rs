f4e5fe86029fdb9d809d0111782fe9ada6345723
amigin <amigin@gmail.com>
2023-06-05 14:07:18 +0300

    Debugging

--- src/generate_e_tag_methods.rs
+++ src/generate_e_tag_methods.rs
@@ -1,12 +1,14 @@
+use std::str::FromStr;
+
 pub fn generate_e_tag_methods() -> proc_macro2::TokenStream {
     quote::quote! {
-        fn get_e_tag_update_field_name() -> Option<&'static str>{
+        fn get_e_tag_field_name() -> Option<&'static str>{
             None
         }
-        fn get_e_tag_update_value(&self) -> Option<i64>{
+        fn get_e_tag_value(&self) -> Option<i64>{
             None
         }
-        fn set_e_tag_update_value(&self, value: i64){
+        fn set_e_tag_value(&self, value: i64){
             None
         }
     }
