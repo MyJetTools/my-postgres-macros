ce65e9349a441bf7edcd50f22a36833d50b66531
amigin <amigin@gmail.com>
2023-09-01 18:43:30 +0300

    Debugging Generics

--- src/fn_impl_select/fn_from.rs
+++ src/fn_impl_select/fn_from.rs
@@ -13,10 +13,10 @@ pub fn fn_from(fields: &[StructProperty]) -> Result<Vec<proc_macro2::TokenStream
         let metadata = field.get_field_metadata()?;
 
         let reading = if let PropertyType::OptionOf(sub_prop) = &field.ty {
-            let type_ident = sub_prop.get_token_stream();
+            let type_ident = sub_prop.get_token_stream_with_generics();
             quote!(#type_ident::from_db_row_opt(db_row, #db_column_name, &#metadata))
         } else {
-            let type_ident = field.ty.get_token_stream();
+            let type_ident = field.ty.get_token_stream_with_generics();
             quote!(#type_ident::from_db_row(db_row, #db_column_name, &#metadata))
         };
 
